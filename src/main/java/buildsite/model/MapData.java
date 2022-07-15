b7312b9a9b3b8f4a9f3ef4014b8663b03cb50b96
polor <1411667243@qq.com0>
2022-07-15 15:57:39 +0800

    ok

--- src/main/java/buildsite/model/MapData.java
+++ src/main/java/buildsite/model/MapData.java
@@ -1,10 +1,30 @@
 package buildsite.model;
 
-public class Map {
+import java.util.List;
+
+public class MapData {
 
     private Environment environment;
     private Water water;
     private String id;
+    private List<String> points;
+    private String radius;
+
+    public List<String> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<String> points) {
+        this.points = points;
+    }
+
+    public String getRadius() {
+        return radius;
+    }
+
+    public void setRadius(String radius) {
+        this.radius = radius;
+    }
 
     public Environment getEnvironment() {
         return environment;
