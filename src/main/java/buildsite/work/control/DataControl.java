726177068579b68442da17045769251c4219c0fb
polor <1411667243@qq.com0>
2022-07-19 23:18:09 +0800

    OK

--- src/main/java/buildsite/work/control/DataControl.java
+++ src/main/java/buildsite/work/control/DataControl.java
@@ -9,8 +9,12 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,23 +33,42 @@ public class DataControl {
 
     private static String id = "0";
 
+    @PostConstruct
+    public void init(){
+        LOG.info("DataControl 启动啦");
+        LOG.info("DataControl 注入啦");
+    }
 
     @PostMapping("/saveCircle")
     @ResponseBody
-    private void saveCircle(String lng, String lat, String radius){
+    private Map saveCircle(@RequestParam String lng, @RequestParam String lat, @RequestParam String radius, HttpServletRequest request, HttpServletResponse response){
 
+        LOG.info("数据上传调用");
+        Map returnData = new HashMap();
         Map<String, String> point = new HashMap<>();
-        point.put(lng, lat);
-        MapData mapData = new MapData();
+        if (lng != null && lat != null && radius != null) {
+            point.put(lng, lat);
+            MapData mapData = new MapData();
 
-        List<Map<String, String>> points = new ArrayList<>();
-        points.add(point);
-        mapData.setId(id);
-        mapData.setPoints(points);
-        mapData.setRadius(radius);
-        mongoTemplate.insert(mapData);
-        id = id + 1;
+            List<Map<String, String>> points = new ArrayList<>();
+            points.add(point);
+            mapData.setId(id);
+            mapData.setPoints(points);
+            mapData.setRadius(radius);
+            mongoTemplate.insert(mapData);
+            id = id + 1;
+            LOG.info("data put success");
 
+            returnData.put("result", true);
+            returnData.put("message", "data push successful");
+            return returnData;
+        }else {
+            returnData.put("result", false);
+            returnData.put("message", "data push not correct");
+            LOG.error("data push false");
+            return returnData;
+
+        }
     }
 
     @PostMapping("/savaPoints")
