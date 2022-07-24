5f9bf07b35384c4f2530ba479b10a7e997722f43
polor <1411667243@qq.com0>
2022-07-24 11:28:23 +0800

    try

--- src/main/java/buildsite/work/control/MainControl.java
+++ src/main/java/buildsite/work/control/MainControl.java
@@ -1,16 +1,26 @@
 package buildsite.work.control;
 
 
+import buildsite.model.MapData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.annotation.PostConstruct;
+import java.util.List;
+
 @Controller
 public class MainControl {
 
     private static final Logger LOG = LoggerFactory.getLogger(MainControl.class);
+
+    @PostConstruct
+    public void init(){
+        LOG.info("MainControl 启动啦");
+        LOG.info("MainControl 注入啦");
+    }
 
     @GetMapping("/index")
     private String index(Model model){
